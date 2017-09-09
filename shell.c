af5e515405a8a1d15ae589f1d249aeef9e9e92ff
zhaoyufei94 <31743926+zhaoyufei94@users.noreply.github.com>
2017-09-09 12:45:59 -0400

    Update shell.c
    non-support for command with space.
    set 10 as the maximum of command line arguments.

--- shell.c
+++ shell.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-
+	int cmd_num = 0;
 	char *cmd;
 	size_t buffersize = 20;
 	size_t length = 0;
-    /*
+        /*
 	cmd = (char *)malloc(buffersize * sizeof(char)); 
 	if(cmd = null){
 		perror("buffer allocation error!\n");
@@ -14,7 +14,7 @@ int main(){
 	}
 	*/
 
-	while(1){
+	while(cmd_num < 10){
 		cmd = (char *)malloc(buffersize * sizeof(char)); 
 		if(cmd = NULL){
 			perror("buffer allocation error!\n");
@@ -25,10 +25,17 @@ int main(){
 		
 		length = getline(&cmd, &buffersize, stdin);
 		cmd[--length] = '\0';
+		for(int i = 0; i < length; i++)
+			if(cmd[i] == 32){
+				printf("error: Command cannot include space. Please re-enter.\n");
+				break;
+			}
+		if(i < length) continue;
 
 //		printf("%zu characters were read.\n",length);
 // 		printf("You typed: '%s'\n",cmd);
 
 		free(cmd);
+		cmd_num ++;
 	}
 }
