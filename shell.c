25bc403ff4c1e0c635410985e22bd30d481a917e
zhaoyufei94 <31743926+zhaoyufei94@users.noreply.github.com>
2017-09-09 21:23:42 -0400

    Execute executable commands
    complete part1

--- shell.c
+++ shell.c
@@ -1,41 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(){
-	int cmd_num = 0, i = 0;
-	char *cmd;
-	size_t buffersize = 20;
-	size_t length = 0;
+    int cmd_num = 0, i = 0;
+    char *cmd;
+    char *tmp_cmd;
+    size_t buffersize = 20;
+    size_t length = 0;
+
+    while(cmd_num < 10){
+        cmd = (char *)malloc(buffersize * sizeof(char));
+        if(cmd == NULL){
+            perror("buffer allocation error!\n");
+            exit(1);
+        }
+        
+        printf("$");
+        
+        length = getline(&cmd, &buffersize, stdin);
+        cmd[--length] = '\0';
+        for(i = 0; i < length; i++)
+            if(cmd[i] == 32){
+                printf("error: Command cannot include space. Please re-enter.\n");
+                break;
+            }
+        if(i < length) continue;
+        
         /*
-	cmd = (char *)malloc(buffersize * sizeof(char)); 
-	if(cmd = null){
-		perror("buffer allocation error!\n");
-		exit();
-	}
-	*/
+        printf("%zu characters were read.\n",length);
+        printf("You typed: '%s'\n",cmd);
+        */
 
-	while(cmd_num < 10){
-		cmd = (char *)malloc(buffersize * sizeof(char)); 
-		if(cmd == NULL){
-			perror("buffer allocation error!\n");
-			exit(1);
-		}
+        if(!strcmp(cmd, "exit")){
+        	printf("exit!\n");
+        	return 0;
+        }
 
-		printf("$");
-		
-		length = getline(&cmd, &buffersize, stdin);
-		cmd[--length] = '\0';
-		for(i = 0; i < length; i++)
-			if(cmd[i] == 32){
-				printf("error: Command cannot include space. Please re-enter.\n");
-				break;
-			}
-		if(i < length) continue;
-
-//		printf("%zu characters were read.\n",length);
-// 		printf("You typed: '%s'\n",cmd);
-
-		free(cmd);
-		cmd_num ++;
-	}
+        tmp_cmd = (char *)malloc((length+2) * sizeof(char));
+        strcpy(tmp_cmd, "./");
+        strcat(tmp_cmd, cmd);
+        		
+        if(fork() == 0){
+        	char *args[]={tmp_cmd,NULL};
+       		execvp(args[0],args);
+        	return 0;
+        }
+        	        
+        free(cmd);
+        cmd_num ++;
+    }
 }
