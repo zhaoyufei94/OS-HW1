d79926e4c6a1e66388eb1807bc93d692374dcad4
zhaoyufei94 <31743926+zhaoyufei94@users.noreply.github.com>
2017-09-10 22:20:46 -0400

    complete history command & fix flaws on cd command
    part2 finished!

--- shell.c
+++ shell.c
@@ -1,3 +1,6 @@
+#define HISTORY 100
+#define MAX_NUM 300
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -5,16 +8,20 @@
 #include <string.h>
 
 int main(){
-    int cmd_num = 0, i = 0;
-    char *cmd[100];
+    int cmd_num = 0, cmd_n = 0, i = 0;
+    char *cmd[HISTORY];
     char *tmp_cmd;
     size_t buffersize = 20;
     size_t length = 0;
-    int his_num = 0;
+    int his_ful = 0;
 
-    while(cmd_num < 200){
+    while(cmd_n < MAX_NUM){
         
-        
+        if(cmd_num == HISTORY){
+        	his_ful = 1;
+        	cmd_num = 0;
+        }
+
         printf("$");
         
         /*read a line from stdin*/
@@ -55,33 +62,75 @@ int main(){
         	//	system("ls");
         	}
         	cmd_num++;
+        	cmd_n++;
         	continue;
         }
 		
         /*build-in command history*/
 		if(!strncmp(cmd[cmd_num], "history", 7)){
-			if(length == 7)
-				for(i = 0; i <= cmd_num; i++)
-					printf("%d %s\n", i, cmd[i]);
-			else if(!strncmp(cmd[cmd_num], "history ", 8)){
-				tmp_cmd = (char *)malloc((length - 8) * sizeof(char));
-				strcpy(tmp_cmd, (cmd[cmd_num] + 8));
-				if(length == 9){
-					i = (int) tmp_cmd[0] - 48;
-					if(i > cmd_num)
-						printf("there is less than %d records in command history\n", i);
-					else printf("%d %s\n", i, cmd[i]);
+			if(!strcmp(cmd[cmd_num], "history -c")){
+				cmd_num = -1;
+				his_ful = 0;
+				printf("History cleared!\n");
+			}
+			else{
+				if(!his_ful){
+					if(length == 7)
+						for(i = 0; i <= cmd_num; i++)
+							printf("%d %s\n", i, cmd[i]);
+					else if(!strncmp(cmd[cmd_num], "history ", 8)){
+						tmp_cmd = (char *)malloc((length - 8) * sizeof(char));
+						strcpy(tmp_cmd, (cmd[cmd_num] + 8));
+						
+						if(length == 9){
+							i = (int) tmp_cmd[0] - 48;
+							if(i > cmd_num)
+								printf("there is less than %d records in command history\n", i);
+							else printf("%d %s\n", i, cmd[i]);
+						}
+						else
+						if(length == 10){
+							i = 10 * (((int) tmp_cmd[0]) - 48) + ((int) tmp_cmd[1]) - 48;
+							if(i > cmd_num)
+								printf("there is less than %d records in command history\n", i);
+							else printf("%d %s\n", i, cmd[i]);
+						}
+						if(length > 10)
+							printf("error: the maximum of history retrieving is 99!\n");
+					}
 				}
-				if(length == 10){
-					i = 10 * (((int) tmp_cmd[0]) - 48) + ((int) tmp_cmd[1]) - 48;
-					if(i > cmd_num)
-						printf("there is less than %d records in command history\n", i);
-					else printf("%d %s\n", i, cmd[i]);
+				else{
+					if(length == 7){
+						for(i = cmd_num + 1; i < HISTORY; i++)
+							printf("%d %s\n", i - cmd_num - 1, cmd[i]);
+						for(i = 0; i < cmd_num + 1; i++)
+							printf("%d %s\n", HISTORY - cmd_num + i - 1, cmd[i]);
+					}
+					else if(!strncmp(cmd[cmd_num], "history ", 8)){
+						tmp_cmd = (char *)malloc((length - 8) * sizeof(char));
+						strcpy(tmp_cmd, (cmd[cmd_num] + 8));
+
+						if(length == 9){
+							i = (int) tmp_cmd[0] - 48;
+							if((i + cmd_num + 1) < HISTORY)
+								printf("%d %s\n", i, cmd[i + cmd_num + 1]);
+							else
+								printf("%d %s\n", i, cmd[i + cmd_num - HISTORY + 1]);
+						}
+						if(length == 10){
+							i = 10 * (((int) tmp_cmd[0]) - 48) + ((int) tmp_cmd[1]) - 48;
+							if((i + cmd_num + 1) < HISTORY)
+								printf("%d %s\n", i, cmd[i + cmd_num + 1]);
+							else
+								printf("%d %s\n", i, cmd[i + cmd_num - HISTORY + 1]);
+						}
+						if(length > 10)
+							printf("error: the maximum of history retrieving is 99!\n");
+					}
 				}
-				if(length > 10)
-					printf("error: the maximum of history retrieving is 99!\n");
 			}
 			cmd_num++;
+			cmd_n++;
 			continue;
 		}
 
@@ -93,6 +142,7 @@ int main(){
             }
         if(i < length){
         	cmd_num++;
+        	cmd_n++;
         	continue;
         }
         
@@ -112,6 +162,7 @@ int main(){
         else
         	printf("process execution error!\n");
         	        
-        cmd_num ++;
+        cmd_num++;
+        cmd_n++;
     }
 }
